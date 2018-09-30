bf66b9972c7e4365ee7dcefaec12d8ba0052b556
Noah <ninjayek@gmail.com>
2018-09-30 22:04:03 +0800

    first Commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
+
+// This is the Link API
+import Link from 'next/link'
+
 const Index = () => (
+  
   <div>
-    <p>Hello Next.js</p>
+    <h1 style={{color:"blue"}}>
+    hey there how are you?
+    </h1>
+    <Link href="/about">
+      <a style={{ fontSize: 20 }}>About Page</a>
+    </Link>
+
+    {/* <Link href="/about" style={{ fontSize: 20 }}>
+  <a>About Page</a>
+    </Link> */}
+
+    <p>tataHello Next.js</p>
   </div>
 )
 
